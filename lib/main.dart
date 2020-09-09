d75f0ee8892051be17674a6f83a3e045a96b2835
iamvpa <iamvpa@users.noreply.github.com>
2020-09-09 20:41:23 +0530

    upd2

--- lib/main.dart
+++ lib/main.dart
@@ -35,24 +35,56 @@ class HomeScreen extends StatelessWidget {
           ClipPath(
             clipper: MyClipper(),
             child: Container(
-                height: 350,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color(0xFF3383CD),
-                      Color(0xFF11249F),
-                    ],
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage("assets\images\virus.png"),
-                  ),
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F),
+                  ],
                 ),
-                child: Column(
-                  children: <Widget>[SvgPicture.asset()],
-                )),
+                image: DecorationImage(
+                  image: AssetImage("assets\images\virus.png"),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: SvgPicture.asset("assets\icons\menu.svg"),
+                  ),
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        SvgPicture.asset(
+                          "assets\icons\coronadr.svg",
+                          width: 230,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                        ),
+                        Positioned(
+                          top: 20,
+                          left: 150,
+                          child: Text(
+                            "All you should do is stay at home.",
+                            style: kHeadingTextStyle.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           )
         ],
       ),
