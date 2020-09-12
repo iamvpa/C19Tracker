942aa2ad8e5fd7ee8965f5251e0fcaac42b86378
iamvpa <iamvpa@users.noreply.github.com>
2020-09-12 22:06:24 +0530

    0d88

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,24 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Color(0xFFE5E5E5),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset("assets\icons\maps-and-flags.svg"),
+              ],
+            ),
           )
         ],
       ),
